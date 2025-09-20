9223778156d4d2293a329b2042a3ed9628383c95
YuIsaac11 <yuisaac11@yahoo.com>
2025-09-20 16:22:57 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+print('Hello, world!')
+print('What is your name?') # Ask for their name
+my_name = input('-->')
+print('Yooo, waaassup,' + my_name)
+print('The number of characters in your answer is:')
+print(len(my_name))
+print('What is your age?') # Ask for their age
+my_age = input('-->')
+print('Awesome! You will be ' + str(int(my_age) + 1) + ' in a year')
